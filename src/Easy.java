4cf7532d6c873c2e25c372f871a6f0c14f03e1c9
alysonlucn <alysonlucn@gmail.com>
2025-09-12 14:42:48 -0300

    Novo commit

--- src/Easy.java
+++ src/Easy.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Easy {
     //1. Two Sum
     public int[] twoSum(int[] nums, int target) {
@@ -25,5 +28,36 @@ public class Easy {
         }
 
         return false;
+    }
+
+    //3. Roman to Integer
+    public int romanToInt(String s) {
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int total = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            int current = romanMap.get(s.charAt(i));
+
+            if((i + 1) < s.length()) {
+                int next = romanMap.get(s.charAt(i + 1));
+
+                if(current < next) {
+                    total -= current;
+                } else {
+                    total += current;
+                }
+            } else {
+                total += current;
+            }
+        }
+        return total;
     }
 }
