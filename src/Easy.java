5bc48916d49558c187ee3a32bd9e6f0c757820e6
alysonlucn <alysonlucn@gmail.com>
2025-09-12 16:05:57 -0300

    Longest Common Prefix

--- src/Easy.java
+++ src/Easy.java
@@ -60,4 +60,18 @@ public class Easy {
         }
         return total;
     }
+
+    //4. Longest Common Prefix
+    public String longestCommonPrefix(String[] strs) {
+        String word = strs[0];
+
+        for(int i = 1; i < strs.length; i++) {
+            while(strs[i].indexOf(word) != 0) {
+                word = word.substring(0, word.length() -1);
+
+                if(word.isEmpty()) return "";
+            }
+        }
+        return word;
+    }
 }
