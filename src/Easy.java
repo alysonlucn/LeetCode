7c318f11ff8ce69a12e0f00091c86539c98ad27b
alysonlucn <alysonlucn@gmail.com>
2025-09-20 12:36:30 -0300

    Single Number

--- src/Easy.java
+++ src/Easy.java
@@ -191,4 +191,13 @@ public class Easy {
         result[0] = 1;
         return result;
     }
+
+    //12. Single Number
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
 }
